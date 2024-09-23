68be581e70cba0433779d54320e7564d5fed8ac4
Goobergreve09 <gregory.greve@yahoo.com>
2024-09-22 22:17:05 -0400

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI_LOCAL || 'mongodb://127.0.0.1:27017/javascriptquiz');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/javascriptquiz');
 
 module.exports = mongoose.connection;
