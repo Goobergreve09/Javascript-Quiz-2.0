d8a2cfff3a03b41cb0773bbf16e18efe1db12827
Goobergreve09 <gregory.greve@yahoo.com>
2024-10-23 14:30:35 -0400

    push

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const startApolloServer = async () => {
     context: authMiddleware
   }));
   
-  // If we're in production, serve client/dist as static assets/
+  // If we're in production, serve client/dist as static assets//
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
